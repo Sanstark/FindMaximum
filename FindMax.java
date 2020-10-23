48f567dfda6bcfd337a54cdfa92d13510826051c
Sanstark <47468504+Sanstark@users.noreply.github.com>
2020-10-23 09:50:57 +0530

    Add files via upload

---
+++ FindMax.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class FindMax <E extends Comparable> {
+    E e1, e2, e3;
+    static Scanner sc = new Scanner(System.in);
+
+    public FindMax(E e1, E e2, E e3){
+        this.e1 = e1;
+        this.e2 = e2;
+        this.e3 = e3;
+    }
+
+    public E testMaximum(){
+        return testMaximum(e1, e2, e3);
+    }
+
+    public static <E extends  Comparable> E testMaximum(E...e1){
+        if(e1 == null) {
+            System.out.println("There are no elements.");
+            return null;
+        }else {
+            Collections.sort(Arrays.asList(e1));
+            printMax(e1[e1.length-1]);
+            return e1[e1.length -1];
+        }
+    }
+
+    public static <E> void printMax(E element){
+        System.out.println("Maximum value : " + element);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Find Maximum Program");
+        FindMax findMax;
+
+        System.out.println("Enter the three numbers : ");
+        Integer num1 = sc.nextInt();
+        Integer num2 = sc.nextInt();
+        Integer num3 = sc.nextInt();
+        findMax = new FindMax(num1, num2, num3);
+        findMax.testMaximum();
+
+        System.out.println("Enter the three numbers : ");
+        Float num4 = sc.nextFloat();
+        Float num5 = sc.nextFloat();
+        Float num6 = sc.nextFloat();
+        findMax = new FindMax(num4, num5, num6);
+        findMax.testMaximum();
+
+        System.out.println("Enter the three strings : ");
+        String str1 = sc.next();
+        String str2 = sc.next();
+        String str3 = sc.next();
+        findMax = new FindMax(str1, str2, str3);
+        findMax.testMaximum();
+    }
+}
