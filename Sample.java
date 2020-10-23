3f603af6b81df3cbd60e6caff12dfb3f6fda268c
Sanstark <47468504+Sanstark@users.noreply.github.com>
2020-10-23 10:18:38 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class Sample {
+
+    public static int add(int...a){
+        return  a[0] +a[1] ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add());
+    }
+}
